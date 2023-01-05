d178b03be26a00e22fdb03842d2724ebf67d340b
adshafqat <adshafqat@yahoo.com>
2023-01-05 19:16:00 +0000

    Update ConfigController.java

--- src/main/java/com/zaynsolutions/config/ConfigController.java
+++ src/main/java/com/zaynsolutions/config/ConfigController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class ConfigController {
@@ -19,10 +19,12 @@ public class ConfigController {
 		this.BackendserverProperties = BackendserverProperties;
 	    }
 
+	@Value("${transformer.mode}")
+   	private String urlvalue;
 	
 	@GetMapping("/")
 	public String returnResponse() {
-		System.out.println("This is a dummy project. URL:"+BackendserverProperties.getUrl());		
+		System.out.println("This is a dummy project. URL:"+urlvalue+" URL:"+BackendserverProperties.getUrl());		
 		return "This is a test application. URL value is :"+BackendserverProperties.getUrl()+" Port:"+BackendserverProperties.getPort();
 	}
 
