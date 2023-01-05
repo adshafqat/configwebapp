8150868826afb030258dc752d4361847433c10ab
adshafqat <adshafqat@yahoo.com>
2023-01-05 18:29:34 +0000

    Update BackendProperties.java

--- src/main/java/com/zaynsolutions/config/BackendProperties.java
+++ src/main/java/com/zaynsolutions/config/BackendProperties.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Component
+
 @Configuration
 @EnableConfigurationProperties
 @ConfigurationProperties("backendserver")
