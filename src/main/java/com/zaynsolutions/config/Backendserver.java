d2ef4780e303a51f2fe1da324261b191dd339f62
adshafqat <adshafqat@yahoo.com>
2023-01-05 18:50:48 +0000

    Create Backendserver.java

---
+++ src/main/java/com/zaynsolutions/config/Backendserver.java
@@ -0,0 +1,17 @@
+
+public class Backendserver {
+
+    private final String content;
+
+    public Backendserver() {
+        this.content = null;
+    }
+
+    public Backendserver(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
