78a542ec28bee4fdceadfc13c20b0a518a6eab10
adshafqat <adshafqat@yahoo.com>
2023-01-05 15:59:35 +0000

    Update ConfigApplication.java

--- src/main/java/com/zaynsolutions/config/ConfigApplication.java
+++ src/main/java/com/zaynsolutions/config/ConfigApplication.java
@@ -7,14 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 public class ConfigApplication {
 
 
-	@Value("${url}")
-    private static String url; 
-
-	@Value("${port}")
-    private static String port; 
-
 	public static void main(String[] args) {
-		System.out.println("Here I am going to print the content of Config Map. URL:"+url+", port:"+port);
 		SpringApplication.run(ConfigApplication.class, args);
 	}
 
