0d038d6fcf0b7c5a3aa6b9ad66960c37c5ac3f61
adshafqat <adshafqat@yahoo.com>
2023-01-05 17:50:18 +0000

    Update ConfigController.java

--- src/main/java/com/zaynsolutions/config/ConfigController.java
+++ src/main/java/com/zaynsolutions/config/ConfigController.java
@@ -4,11 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
-import java.time.format.DateTimeFormatter;  
-import java.time.LocalDateTime; 
-import java.lang.Math;
-import java.util.concurrent.*;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @RestController
